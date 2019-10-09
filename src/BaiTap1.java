5bcaf4bbbb4a2ff1f836c36015c07f229568add6
nguyenthininh <45448845+nguyenthininh@users.noreply.github.com>
2019-10-09 12:42:28 +0700

    nopbaitap

--- src/BaiTap1.java
+++ src/BaiTap1.java
@@ -5,14 +5,14 @@ public class BaiTap1 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         int n;
         System.out.println("nhap n :");
-        Scanner sc = new Scanner(System.in);
         n= sc.nextInt();
 
         System.out.println("cac so nguyen to nho hon "+n+" la :");
         for(int i =1; i<n; i++){
-            int count==0;
+            int count=0;
             for(int j=1; j<i;j++){
                 if(i%j==0){
                     count++;
